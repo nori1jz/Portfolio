b29e0edd2c2502091f52a686fe60306d81fd98a6
nori1jz <apriotype2xxnorixx@gmail.com>
2021-10-13 08:05:45 +0000

    [add]route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'public/homes#top'
+  
+  root  'public/homes#top'
+  
   devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
@@ -11,5 +13,26 @@ Rails.application.routes.draw do
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
 }
+
+  namespace :admin do
+     resources :items, only: [:new, :create, :index, :show, :edit, :update]
+     resources :makers, only: [:new, :create, :index, :show, :edit, :update]
+     resources :genres, only: [:new, :create, :index, :edit, :update]
+     resources :customers, only: [:index, :show, :edit, :update]
+  end
+  
+  namespace :public do
+     resources :items, only: [:index, :show]
+     resources :makers, only: [:index, :show]
+     resources :contacts, only: [:new, :create]
+     post 'contacts/confirm' => 'contacts#confirm', as: 'confirm'
+     post 'contacts/back' => 'contacts#back', as: 'back'
+     get  'done' => 'contacts#done', as: 'done'
+  end   
+      
+
+  
+  
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
