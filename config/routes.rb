06cde65091aadeea8f07f91f8a3d973190e2ddb5
nori1jz <apriotype2xxnorixx@gmail.com>
2021-10-21 05:44:27 +0000

    [Finish]退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root  'public/homes#top'
- 
-  # namespace :public do
-  #   get 'unsubscribe' => 'homes#unsubscribe', as: 'confirm_unsubscribe'
-  #   patch ':id/withdraw' => 'homes#withdraw', as: 'withdraw_customer'
-  #   put 'withdraw' => 'customers#withdraw'
-     resource :customer, only: [:show, :edit, :update] do
-       collection do
-         get 'unsubscribe' => 'homes#unsubscribe', as: 'confirm_unsubscribe'
-         patch 'withdraw' => 'homes#withdraw', as: 'withdraw_customer'
-          put 'withdraw' => 'customers#withdraw'
-    
-       end
-     end
-  
-  
+
+
+
   devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
@@ -34,21 +22,26 @@ Rails.application.routes.draw do
      resources :genres, only: [:new, :create, :index, :edit, :update]
      resources :customers, only: [:index, :show, :edit, :update]
   end
-  
+
   namespace :public do
      resources :items, only: [:index, :show]
      resources :makers, only: [:index, :show]
      resources :contacts, only: [:new, :create]
-     resources :customers, only: [:show, :edit]
+     resources :customers, only: [:show, :edit, :update] do
+       member do
+         get :unsubscribe
+         patch :withdraw
+       end
+     end
      get 'homes/about' => 'homes#about'
      post 'contacts/confirm' => 'contacts#confirm', as: 'confirm'
      post 'contacts/back' => 'contacts#back', as: 'back'
      get  'done' => 'contacts#done', as: 'done'
-  end   
-      
+  end
 
-  
-  
- 
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
